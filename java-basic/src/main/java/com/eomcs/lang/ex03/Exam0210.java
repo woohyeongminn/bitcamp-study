16ad081177d7fb317fccc6939e40ffe9063a5922
woohyeongminn <gudals.woo@gmail.com>
2021-06-30 20:12:45 +0900

    ok

---
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0210.java
@@ -0,0 +1,15 @@
+package com.eomcs.lang.ex03;
+public class Exam0210 {
+    public static void main(String[] args) {
+        System.out.println(100);
+        System.out.println(0144);
+        System.out.println(0b1100100);
+        System.out.println(0B1100100);
+        System.out.println(0b01100100);
+        System.out.println(0B01100100);
+        System.out.println(0x64);
+        System.out.println(0X64);
+        System.out.println(0x064);
+        System.out.println(0X064);
+    }
+}
