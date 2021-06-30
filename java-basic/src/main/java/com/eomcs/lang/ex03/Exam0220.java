da02db573eb357a563b126995344313c17bc7d07
woohyeongminn <gudals.woo@gmail.com>
2021-06-30 20:30:46 +0900

    ok2

---
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0220.java
@@ -0,0 +1,20 @@
+package com.eomcs.lang.ex03;
+public class Exam0220 {
+    public static void main(String[] args) {
+        System.out.println(12783406);
+        System.out.println(1278-3406);
+        System.out.println(12_783_406);
+        //숫자 맨 앞 또는 맨 뒤에 삽입할 수 없다.
+
+        System.out.println(077);
+        System.out.println(0_77);
+        System.out.println(07_7);
+
+        System.out.println(0b1100100);
+        System.out.println(0b110_0100);
+        System.out.println(0b1_1_0_0_1_0_0);
+
+        System.out.println(0xffaa);
+        System.out.println(0xff_aa);
+    }
+}
