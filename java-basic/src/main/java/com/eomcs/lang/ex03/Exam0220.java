c8f90078a030791456ebb6770c275103f92f073e
woohyeongminn <gudals.woo@gmail.com>
2021-07-05 19:58:48 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex03/Exam0220.java
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0220.java
@@ -5,7 +5,7 @@ package com.eomcs.lang.ex03;
 //
 public class Exam0220 {
   public static void main(String[] args) {
-    
+
     //## 10진수에 _ 문자를 삽입하기
     System.out.println(12783406);
     System.out.println(1278_3406);
@@ -14,7 +14,7 @@ public class Exam0220 {
     //숫자 맨 앞 또는 맨 뒤에 삽입할 수 없다.
     //System.out.println(_12783406); 
     //System.out.println(12783406_);
-    
+
     //## 8진수에 _ 문자를 삽입하기
     System.out.println(077);
     System.out.println(0_77);
