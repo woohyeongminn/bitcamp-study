341a0427c5a8ef693889a6746b0f8afde2064951
woohyeongminn <gudals.woo@gmail.com>
2021-10-29 16:01:01 +0900

    test

---
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberAddController.java
@@ -0,0 +1,69 @@
+package com.eomcs.pms.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
+import com.eomcs.pms.dao.MemberDao;
+import com.eomcs.pms.domain.Member;
+
+
+@WebServlet("/member/add")
+public class MemberAddController extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  SqlSession sqlSession;
+  MemberDao memberDao;
+
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
+    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
+    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
+  }
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    Member member = new Member();
+
+    member.setName(request.getParameter("name"));
+    member.setEmail(request.getParameter("email"));
+    member.setPassword(request.getParameter("password"));
+    member.setPhoto(request.getParameter("photo"));
+    member.setTel(request.getParameter("tel"));
+
+    try {
+      memberDao.insert(member);
+      sqlSession.commit();
+
+      // 리프래시(refresh)
+      // 웹브라우저에게 서버가 보내준 HTML을 출력한 후 
+      // 1초가 경과하면 지정한 URL을 다시 요청하도록 명령한다.
+      response.setHeader("Refresh", "1;url=list");
+      request.getRequestDispatcher("MemberAdd.jsp").forward(request, response);
+
+    } catch (Exception e) {
+      // 오츄를 출력할 때 사용할 수 있도록 예외 객체를 저장소에 보관한다.
+      request.setAttribute("error", e);
+
+      // 오류가 발생하면, 오류 내용을 출력할 뷰를 호출한다.
+      request.getRequestDispatcher("/Error.jsp").forward(request, response);
+    }
+
+
+  }
+}
+
+
+
+
+
+
+
---
+++ study-project/app-server/src/main/webapp/member/MemberList.jsp
@@ -0,0 +1,39 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"
+    trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<!DOCTYPE html>
+<html>
+<head>
+  <title>회원목록</title>
+</head>
+<body>
+<h1>회원 목록(MVC + EL + JSTL)</h1>
+<a href='form'>새회원</a><br>
+<table border='1'>
+<thead>
+  <tr>
+    <th>번호</th>
+    <th>이름</th>
+    <th>이메일</th>
+    <th>전화</th>
+    <th>등록일</th>
+  </tr>
+</thead>
+<tbody>
+
+<c:forEach items="${memberList}" var="member">
+<tr>
+    <td>${member.no}</td>
+    <td><a href='detail?no=${member.no}'>${member.name}</a></td> 
+    <td>${member.email}</td> 
+    <td>${member.tel}</td> 
+    <td>${member.registeredDate}</td>
+</tr>
+</c:forEach>
+
+</tbody>
+</table>
+</body>
+</html>
+
